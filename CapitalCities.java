84671ae47698ebdd614bc763f0f0c17578ce6bf5
Shashwat Kumar Singh <73544122+harryalbrus272@users.noreply.github.com>
2021-02-21 14:28:04 +0530

    added more functions to the hashmaps

--- CapitalCities.java
+++ CapitalCities.java
@@ -20,10 +20,13 @@ public class CapitalCities {
         capitalCities.put("Australia", "Sydney");
         capitalCities.put("USA", "Washington DC");
         capitalCities.put("Pakistan", "Islamabad");
+        capitalCities.put("Nepal", "Kathmandu");
+        capitalCities.put("Germany", "Munich");
         System.out.println(capitalCities);
-        //get is used to find a value using its key
+        //get is used to find a value using its key. Returns null when nothing is found associated to the key
         System.out.println("get Function  " + capitalCities.get("India"));
-        //Remove an Item - use a .remove() function
+        System.out.println("get Function  " + capitalCities.get("Myanmar"));
+        //Remove an Item - use a .remove() function. It return the value that has been deleted corresponding to the keys.
         System.out.println(".remove function  "+capitalCities.remove("Pakistan"));
         System.out.println(capitalCities);
         
@@ -33,6 +36,52 @@ public class CapitalCities {
         capitalCities.keySet().forEach((item) -> {
             System.out.print(item+" ");
         });
+        capitalCities.entrySet().forEach((items) -> {System.out.println(items);});
+        System.out.println();
+        System.out.println("getOrDefault() "+capitalCities.getOrDefault("China", "NotFound"));
+        
+        //using replace for the updating the hashmap keys
+        capitalCities.replace("Germany", "Berlin");
+        System.out.println(capitalCities);
+        //replaceAll wants a Bi-function as a input
+        capitalCities.replaceAll((key, value) -> value.toUpperCase());
+        System.out.println(capitalCities);
+        
+        //cloning the hashmap
+        HashMap <String, String> cloneHashMap = new HashMap<>();
+        cloneHashMap = (HashMap<String, String>) capitalCities.clone();
+        System.out.println("cloneHashMap "+cloneHashMap);
+        
+        //contains function containsKey(), containsValue(), isEmpty()
+        System.out.println("containsKey() "+ capitalCities.containsKey("India") + " containsValue() "+capitalCities.containsValue("Delhi")+ " isEmpty() "+capitalCities.isEmpty());
+        
+        //merge function  o -> stands for older value of hashmap, n -> stands for new value
+        //Better use of merge function is :-
+        /* prices2.forEach((key, value) -> prices1.merge(key, value, (oldValue, newValue) -> {
+      
+        // return the smaller value
+        if (oldValue < newValue) {
+          return oldValue;
+        }
+        else {
+          return newValue;
+        }
+      }));*/
+        capitalCities.merge("India", "New Delhi", (o, n) ->{
+            if(o.equalsIgnoreCase(n)){
+                return o;
+            }
+            return  n;
+        });
+        System.out.println(capitalCities);
+        
+        //compute and computeIfAbsent and ComputeIfPresent functions
+        
+        capitalCities.compute("Myanmar", (String key,String value) -> { 
+            key.toLowerCase(); return (null);
+        });
+        System.out.println(capitalCities);
     }
+    
    
 }
