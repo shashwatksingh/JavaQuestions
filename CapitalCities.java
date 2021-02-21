fdb573a8387b2e92b406e729d64c5a2b6b7cdb99
Shashwat Kumar Singh <73544122+harryalbrus272@users.noreply.github.com>
2021-02-21 12:25:27 +0530

    more funcions on hashmaps

--- CapitalCities.java
+++ CapitalCities.java
@@ -19,9 +19,20 @@ public class CapitalCities {
         capitalCities.put("India", "New Delhi");
         capitalCities.put("Australia", "Sydney");
         capitalCities.put("USA", "Washington DC");
+        capitalCities.put("Pakistan", "Islamabad");
         System.out.println(capitalCities);
         //get is used to find a value using its key
-        System.out.println("get Function" + capitalCities.get("India"));
+        System.out.println("get Function  " + capitalCities.get("India"));
+        //Remove an Item - use a .remove() function
+        System.out.println(".remove function  "+capitalCities.remove("Pakistan"));
+        System.out.println(capitalCities);
+        
+        //Iterating through the hashmap using for loop
+        //keyset() return an array of Hashmap. .values() gives an array of all the values. .entryset() return the array of key value pairs.
+        System.out.println(".keySet() " +capitalCities.keySet() + " .values() "+capitalCities.values() + ".entrySet() " + capitalCities.entrySet());
+        capitalCities.keySet().forEach((item) -> {
+            System.out.print(item+" ");
+        });
     }
    
 }
