c7e0bff94ef829dfb23f3e6a0f482e006411e20d
harryalbrus272 <shashwatksingh.27@gmail.com>
2021-04-14 05:35:09 +0100

    overloading remove method on linked list

--- ApplicationLinkedList.java
+++ ApplicationLinkedList.java
@@ -24,14 +24,24 @@ public class ApplicationLinkedList {
         animals.add("Rat");
         animals.add("Goat");
         animals.add("Monkey");
-        
+
+        //The index starts from 0 in the linked list just like arrays
         // Getting a value for the particaular index
         String str = animals.get(3);
         System.out.println("String at position 3 " + str);
-        
+
         //Change the element at the particaular position
         animals.set(3, "Snake");
+        //Remove an element from a particular index
+        System.out.println(animals.remove());
+                
+        //Iterating throught the linked list using for each loop
+        for (String animal : animals) {
+            System.out.print(animal);
+            System.out.print(", ");
+        }
+
         System.out.println("Linked List animals: " + animals);
-        
+
     }
 }
