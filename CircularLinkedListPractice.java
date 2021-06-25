861438716802894f83f76b178e514aabdf70efa7
harryalbrus272 <shashwatksingh.27@gmail.com>
2021-06-25 15:54:10 +0100

    adding an item after an item in a circular list

--- CircularLinkedListPractice.java
+++ CircularLinkedListPractice.java
@@ -10,19 +10,22 @@ package com.mycompany.javaquestions;
  * @author HP
  */
 public class CircularLinkedListPractice {
+
     //inner class for the Node creation
-    public class Node{
-        int data; Node next;
-        public Node(int d){
+    public class Node {
+
+        int data;
+        Node next;
+
+        public Node(int d) {
             data = d;
         }
     }
     Node last;
-    
-    
+
     //displaying the whole circular linked list
-    void display(CircularLinkedListPractice list){
-        if(last == null) {
+    void display(CircularLinkedListPractice list) {
+        if (last == null) {
             System.out.println("List is empty");
             return;
         }
@@ -30,6 +33,20 @@ public class CircularLinkedListPractice {
         do {
             System.out.println(curr.data);
             curr = curr.next;
-        } while(curr!=last.next);
+        } while (curr != last.next);
+    }
+
+    //adding an element at the beginning
+    void addAtbeginning(int data) {
+        Node new_node = new Node(data);
+        new_node.next = last.next;
+        last.next = new_node;
+    }
+    
+    //Adding to an empty circular linked list
+    void addToEmpty (int data) {
+        Node new_node = new Node(data);
+        last = new_node;
+        new_node.next = new_node;
     }
 }
