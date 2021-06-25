0090ac68e9c5c0de343547763b9223f5e9208e6b
harryalbrus272 <shashwatksingh.27@gmail.com>
2021-06-25 14:07:35 +0100

    refined the code fo the circular linked list

--- CircularLinkedListPractice.java
+++ CircularLinkedListPractice.java
@@ -10,5 +10,26 @@ package com.mycompany.javaquestions;
  * @author HP
  */
 public class CircularLinkedListPractice {
+    //inner class for the Node creation
+    public class Node{
+        int data; Node next;
+        public Node(int d){
+            data = d;
+        }
+    }
+    Node last;
     
+    
+    //displaying the whole circular linked list
+    void display(CircularLinkedListPractice list){
+        if(last == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        Node curr = last.next;
+        do {
+            System.out.println(curr.data);
+            curr = curr.next;
+        } while(curr!=last.next);
+    }
 }
